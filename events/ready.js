895692fb8505473fa3fcb320ec7e48a39d4595a6
rooblox <34076536+rooblox@users.noreply.github.com>
2026-03-24 16:33:32 -0400

    Delete events directory

--- events/ready.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ready',
-    once: true,
-    execute(client) {
-        console.log(`${client.user.tag} is online!`);
-        client.user.setActivity('Connor is da goat frfr');
-    },
-};
